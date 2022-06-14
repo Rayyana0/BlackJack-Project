43e4275de42895dd0e1af407189b8c55acbb9803
Peter-Dagley <peter_dagley@outlook.com>
2022-06-14 12:02:03 +0100

    added a random number generator

--- blackjackscript.js
+++ blackjackscript.js
@@ -1,3 +1,16 @@
 'use strict'
 
-console.log("Hello World")
+// Elements of the page
+
+
+// Additional Variables
+
+
+// Functions
+function randomCardNumber() {
+    let randomNumber = Math.floor(Math.random() * 10) + 1;
+    return randomNumber;
+}
+
+let initialCard1 = randomCardNumber();
+let initialCard2 = randomCardNumber();
