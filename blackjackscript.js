91ba768d988b6113a94203abb39beca6e48366d1
Peter-Dagley <peter_dagley@outlook.com>
2022-06-14 13:26:39 +0100

    added draw card function

--- blackjackscript.js
+++ blackjackscript.js
@@ -6,35 +6,43 @@ const startGameButton = document.querySelector("#game");
 const drawCardButton = document.querySelector("#draw");
 const foldHandButton = document.querySelector("#fold");
 const restartGameButton = document.querySelector("#restart");
+const sum = document.querySelector("#message");
 
-// Additional Variables
-let cardList = [];
-let initialCard1;
-let initialCard2;
-
-// Functions
+// Random number generator
 function randomCardNumber() {
     let randomNumber = Math.floor(Math.random() * 10) + 1;
     return randomNumber;
 }
 
+// Additional Variables
+let cardList = [];
+let initialCard1 = randomCardNumber();
+let initialCard2 = randomCardNumber();
+
 
 // Add initial card valules to table method
 let addInitialCards = () => {
-    initialCard1 = randomCardNumber();
-    initialCard2 = randomCardNumber();
-    
+    cardList = [initialCard1, initialCard2];
     cardItem.innerHTML+=initialCard1+"<br>";
     cardItem.innerHTML+=initialCard2+"<br>";
-
-    cardList = [initialCard1, initialCard2];
+    console.log(cardList);
 }
 
-// Sum of numbers
-let sumOfNumbers = () => {
-    addInitialCards()
-    console.log(initialCard1 + initialCard2)
+// Add a card
+let addACard = () => {
+    cardList.push(randomCardNumber());
+    cardItem.innerHTML+=randomCardNumber()+"<br>";
+    console.log(cardList);
 }
+
+// Sum of cardList
+let sumOfCards = () => {
+    for (let i = 0; i < cardList.length; i++) {
+        result += cardList[i];
+    }
+    sum.textContent(result);
+}
+
 
 // Flush the table of cards
 let restartGame = () => {
@@ -46,3 +54,6 @@ startGameButton.addEventListener("click", addInitialCards);
 
 // Restart game
 restartGameButton.addEventListener("click", restartGame);
+
+// Draw a card
+drawCardButton.addEventListener("click", addACard) 
